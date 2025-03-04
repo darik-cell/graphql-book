e34ec734779bd1f5e95c8b5d0ff5ab50d99a3310
Alexander Alenushka <Alexander.Alyonushka@magenta-technology.com>
2025-03-04 19:01:26 +0400

    first commit

---
+++ src/main/java/org/study/graphqldemophoto/GraphqlDemoPhotoApplication.java
@@ -0,0 +1,13 @@
+package org.study.graphqldemophoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDemoPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDemoPhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/study/graphqldemophoto/GraphqlDemoPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.graphqldemophoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
